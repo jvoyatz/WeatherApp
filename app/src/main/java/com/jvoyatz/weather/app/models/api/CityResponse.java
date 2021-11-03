22c8de1ff0eeeaa74459500d4380898730a45cf1
John Voyatzis <j.voyatzis@satways.net>
2021-11-04 01:11:15 +0200

    forgot to add to git CityResponse class

---
+++ app/src/main/java/com/jvoyatz/weather/app/models/api/CityResponse.java
@@ -0,0 +1,10 @@
+package com.jvoyatz.weather.app.models.api;
+
+import com.jvoyatz.weather.app.models.api.city.SearchApi;
+
+public class CityResponse {
+	private SearchApi searchApi;
+	public SearchApi getSearchApi(){
+		return searchApi;
+	}
+}
