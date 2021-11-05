154751fb34deb796e9dd45896c75c583680b6167
John Voyatzis <j.voyatzis@satways.net>
2021-11-05 15:31:07 +0200

    added a base class for transforming remote models to entities

---
+++ app/src/main/java/com/jvoyatz/weather/app/models/converters/TypeConverter.java
@@ -0,0 +1,47 @@
+package com.jvoyatz.weather.app.models.converters;
+
+import com.jvoyatz.weather.app.models.api.city.CitySearchApi;
+import com.jvoyatz.weather.app.models.api.entities.CityEntity;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * Generic class used to transform remote models to local entities
+ * @param <F> the remote model, eg an API Model
+ * @param <T> the local model-entity, used in app
+ *
+ */
+public abstract class TypeConverter<F, T> {
+
+    /**
+     * Abstract method implemented in subclasses.
+     * Creates an entity type from the given argument (FROM type)
+     * @param from the remote model
+     * @return the newly-created local model
+     */
+    public abstract T toEntity(F from);
+
+    /**
+     * Method which takes a collection of remote models
+     * and transforms them into a List of Entities
+     *
+     * @param apiModels the list of remote models
+     * @return a new list containing the entities.
+     */
+    public List<T> toEntities(Collection<F> apiModels){
+        List<T> entities = null;
+        if(apiModels != null){
+            entities = new ArrayList<>(apiModels.size());
+
+            for (F apiModel : apiModels) {
+                T entity = toEntity(apiModel);
+                if(entity != null){
+                    entities.add(entity);
+                }
+            }
+        }
+        return entities;
+    }
+}
