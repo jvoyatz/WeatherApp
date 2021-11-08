630c0723cf8f012d1d327589518b4033703a833f
John Voyatzis <j.voyatzis@satways.net>
2021-11-09 01:41:27 +0200

    injecting typeconverter in db creation function

--- app/src/main/java/com/jvoyatz/weather/app/di/AppModule.java
+++ app/src/main/java/com/jvoyatz/weather/app/di/AppModule.java
@@ -1,10 +1,13 @@
 package com.jvoyatz.weather.app.di;
 
 import android.content.Context;
+import android.util.Log;
 
 import androidx.room.Room;
 
+import com.jvoyatz.weather.app.models.converters.WeatherTypeListConverter;
 import com.jvoyatz.weather.app.storage.db.CityDao;
+import com.jvoyatz.weather.app.storage.db.WeatherDao;
 import com.jvoyatz.weather.app.storage.db.WeatherDatabase;
 
 import javax.inject.Singleton;
@@ -14,6 +17,7 @@ import dagger.Provides;
 import dagger.hilt.InstallIn;
 import dagger.hilt.android.qualifiers.ApplicationContext;
 import dagger.hilt.components.SingletonComponent;
+import timber.log.Timber;
 
 @Module
 @InstallIn(SingletonComponent.class)
@@ -21,12 +25,12 @@ public class AppModule{
 
     @Singleton
     @Provides
-    public static WeatherDatabase providesWeatherDB(@ApplicationContext Context context){
-        WeatherDatabase db = Room.databaseBuilder(context,
-                WeatherDatabase.class, "weather-app-database")
-                //.addMigrations(WeatherDatabase.MIGRATION_1_2)
+    public static WeatherDatabase providesWeatherDB(@ApplicationContext Context context, WeatherTypeListConverter weatherTypeListConverter){
+        Timber.d("providesWeatherDB: " + weatherTypeListConverter.gson);
+        WeatherDatabase db =
+                Room.databaseBuilder(context,WeatherDatabase.class, "weather-app-database")
+                .addTypeConverter(weatherTypeListConverter)
                 .build();
-
         return db;
     }
 
@@ -34,5 +38,11 @@ public class AppModule{
     @Provides
     public static CityDao provideCityDAO(WeatherDatabase weatherDatabase){
         return weatherDatabase.cityDAO();
+    }
+
+    @Singleton
+    @Provides
+    public static WeatherDao provideWeatherDAO(WeatherDatabase weatherDatabase){
+        return weatherDatabase.weatherDAO();
     }
 }
